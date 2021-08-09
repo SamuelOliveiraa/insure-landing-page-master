b4272fa9f803f3fd45387a6fe10e554fd54f72e6
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2021-08-09 20:14:47 -0300

    Arrumei a responsividade do site, ainda falta melhorar

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,6 +1,37 @@
 $(document).ready(function () {
-    $('#hamburguer').click(function() {
-        $(".open").toggleClass("close")
-        $('.list').toggle()
-    })
+    
+    //I USED JQUERY TO SHOW AND HIDE THE MENU HAMBURGUER AND THE DIV WITH THE CLASS "LIST" FOR BIG SCREENS 
+
+    $(window).resize(function() {
+        var vpWidth = $(window).width();
+
+        if( vpWidth <= 770 ){
+            if($("#hamburguer").hasClass("close")){
+                $('#hamburguer').removeClass('close')
+                $("#hamburguer").addClass("open")
+                $('.list').toggle()
+            }else if ($("#hamburguer").hasClass("open")){
+                $('#hamburguer').click(function() {
+                    $(".open").toggleClass("close")
+                    $('.list').toggle()
+                })
+            }
+        }else if(vpWidth > 770){
+            $("#hamburguer").addClass("close")
+            $('.list').show()
+        } 
+    });
+
+    //I USED JQUERY TO SMOOTH PAGE SCROLL
+    $(function(){
+        $('li a').click(function(event) {
+            event.preventDefault(); 
+            var href = $(this).attr('href'); 
+            var offSetTop = $(href).offset().top; 
+            $('html, body').animate({ 
+                'scrollTop': offSetTop 
+            }, 'slow'); 
+        });
+    });
 })
+
